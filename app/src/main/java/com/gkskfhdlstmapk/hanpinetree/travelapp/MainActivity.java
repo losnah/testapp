5e9d12b7b668b9c9ad6d593765931752b79fe223
H S <gkskfhdlstm@naver.com>
2019-02-11 19:07:20 +0900

    simple intro

---
+++ app/src/main/java/com/gkskfhdlstmapk/hanpinetree/travelapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gkskfhdlstmapk.hanpinetree.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Contents~");
+
+
+
+    }
+}
